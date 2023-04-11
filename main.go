fc5575c12d0036627cc71940b02a88928170af22
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2023-04-11 10:04:04 -0300

    add functions and varaible funcition

--- main.go
+++ main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Print("Course")
+
+	sum := sum(1, 2)
+	fmt.Print(sum)
+
+	result := f("Texto da função")
+	fmt.Print(result)
+
+	sumResult, subResult := calculations(10, 20)
+	fmt.Print(sumResult, subResult)
+
+	// Ignore second result
+	sumResult, _ = calculations(10, 20)
+	fmt.Print(sumResult)
+
+	_, subResult = calculations(10, 20)
+	fmt.Print(subResult)
 }
